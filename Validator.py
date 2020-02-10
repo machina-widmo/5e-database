9038e1e89d328ea51079157e488641b0d89a5d18
jon.nabozny <jon.nabozny@epicgames.com>
2020-02-10 00:07:13 -0500

    Tons of fun...

--- Validator.py
+++ Validator.py
@@ -1,12 +1,16 @@
 import fastjsonschema as fjs
 import json
 import os
+import sys
+import traceback
 
 SCRIPT_DIRECTORY = os.path.abspath(os.path.realpath(os.path.dirname(__file__)))
-SCHEMA_DIRECTORY = os.path.join(SCRIPT_DIRECTORY, "Schemas")
+DATABASE_SCHEMA_DIRECTORY = os.path.join(SCRIPT_DIRECTORY, "Schemas/DatabaseSchemas")
+MISC_SCHEMA_DIRECTORY = os.path.join(SCRIPT_DIRECTORY, "Schemas/SupportingSchemas")
 
 
 def load_json_file(path):
+    print(path)
     with open(path) as json_file:
         return json.load(json_file)
 
@@ -17,52 +21,52 @@ DATABASES = {
     if name.endswith(".json")
 }
 
-SCHEMAS = {
-    name: load_json_file(os.path.join(SCHEMA_DIRECTORY, name))
-    for name in os.listdir(SCHEMA_DIRECTORY)
+DATABASE_SCHEMAS = {
+    name: load_json_file(os.path.join(DATABASE_SCHEMA_DIRECTORY, name))
+    for name in os.listdir(DATABASE_SCHEMA_DIRECTORY)
     if name.endswith(".schema.json")
 }
 
-SCHEMA_NAMES_BY_DATABASE = {
-    "5e-SRD-Ability-Scores.json": "AttributeEntry.schema.json",
-    "5e-SRD-BackgroundFeatures.json": "BackgroundFeatureEntry.schema.json",
-    "5e-SRD-Backgrounds.json": "BackgroundEntry.schema.json",
-    "5e-SRD-Classes.json": "ClassEntry.schema.json",
-    "5e-SRD-Conditions.json": "ConditionEntry.schema.json",
-    "5e-SRD-Damage-Types.json": "DamageTypeEntry.schema.json",
-    "5e-SRD-Equipment-Categories.json": "EquipmentCategoryEntry.schema.json",
-    "5e-SRD-Equipment.json": "EquipmentEntry.schema.json",
-    "5e-SRD-Features.json": "FeatureEntry.schema.json",
-    "5e-SRD-Languages.json": "LanguageEntry.schema.json",
-    "5e-SRD-Levels.json": "LevelEntry.schema.json",
-    "5e-SRD-Magic-Schools.json": "MagicSchoolEntry.schema.json",
-    "5e-SRD-Proficiencies.json": "ProficiencyEntry.schema.json",
-    "5e-SRD-Races.json": "RaceEntry.schema.json",
-    "5e-SRD-Skills.json": "SkillEntry.schema.json",
-    "5e-SRD-StartingEquipment.json": "StartingEquipmentEntry.schema.json",
-    "5e-SRD-Subclasses.json": "SubclassEntry.schema.json",
-    "5e-SRD-Subraces.json": "SubraceEntry.schema.json",
-    "5e-SRD-Traits.json": "TraitEntry.schema.json",
-    "5e-SRD-Weapon-Properties.json": "WeaponPropertyEntry.schema.json",
-    # TODO: Monsters
-    # TODO: Spells
-    # TODO: Spellcasting
-    # TODO: Characters
+MISC_SCHEMAS = {
+    name: load_json_file(os.path.join(MISC_SCHEMA_DIRECTORY, name))
+    for name in os.listdir(MISC_SCHEMA_DIRECTORY)
+    if name.endswith(".schema.json")
 }
 
+def get_schema(schema_name):
+    # print(schema_name)
+    schema_name = schema_name.split('/')[-1]
+    if schema_name in MISC_SCHEMAS:
+        return MISC_SCHEMAS[schema_name]
 
-handlers = {'': SCHEMAS.get}
+    elif schema_name in DATABASE_SCHEMAS:
+        return DATABASE_SCHEMAS[schema_name]
 
-for database in DATABASES.keys():
-    schema = SCHEMA_NAMES_BY_DATABASE.get(database)
-    if schema is not None:
-        print(f'Validating database {database} with schema {schema}')
-        database_schema = {
-            "$schema": "http://json-schema.org/draft-07/schema",
-            "type": "array",
-            "items": {
-                "$ref": f"{schema}"
-            }
-        }
+    raise Exception(f'Unable to find schema {schema_name}')
 
-        fjs.validate(database_schema, DATABASES[database], handlers=handlers)
+handlers = {'': get_schema}
+
+for database_name, database in DATABASES.items():
+    database_base_name = database_name.replace('.json', '')
+    database_schema_name = database_base_name + '.schema.json'
+    database_schema = DATABASE_SCHEMAS.get(database_schema_name)
+
+    if not database_schema:
+        print(f'No schema for {database_name}, skipping validation.')
+        continue
+
+    try:
+        fjs.validate(database_schema, database, handlers=handlers)
+
+    except fjs.JsonSchemaException as jsce:
+        print(f'Validating database {database_name} with schema {database_schema_name}')
+        with open(os.path.join(SCRIPT_DIRECTORY, 'generated'), 'w') as tmp_gen:
+            tmp_gen.write(fjs.compile_to_code(database_schema, handlers=handlers))
+        traceback.print_exc()
+        print(f'path={jsce.path}')
+        print()
+
+    except Exception as e:
+        print(f'Validating database {database_name} with schema {database_schema_name}')
+        traceback.print_exc()
+        print()
